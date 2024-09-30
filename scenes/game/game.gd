113df4ffe6088b03c356f94e79f4182a0520b81d
Tobias Pettersson <a.tobias.pettersson@gmail.com>
2024-09-30 13:49:17 +0200

    FIX: Store a copy of the current time
    This commit fixes the previous issue that the time splits referred to
    the current time, meaning they would reflect the clock that was running
    and not infact store the time for when a time_split zone was entered.
    This is now fixed by duplicating the resource, when calling get_time()
    on the stop_watch.

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -23,166 +23,163 @@ var _countdown_timer : countdown_timer = countdown_timer.new()
 var _time_splits : Array[date_time] = []
 
 func _ready() -> void:
-	# Connect signals from ui to the local callbacks
-	ui.user_requested_quit_game.connect(_on_user_requested_quit_game)
-	ui.user_requested_resume_game.connect(_on_user_requested_resume_game)
-	ui.user_requested_start_game.connect(_on_user_requested_start_game)
-	
-	# Add the countdown timer to the tree
-	add_child(_countdown_timer)
-	# Connect countdown timer signals
-	_countdown_timer.on_one_second_hit.connect(_on_one_sec_hit_timer)
-	_countdown_timer.on_countdown_timeout.connect(_on_countdown_timeout_timer)
+    # Connect signals from ui to the local callbacks
+    ui.user_requested_quit_game.connect(_on_user_requested_quit_game)
+    ui.user_requested_resume_game.connect(_on_user_requested_resume_game)
+    ui.user_requested_start_game.connect(_on_user_requested_start_game)
+
+    # Add the countdown timer to the tree
+    add_child(_countdown_timer)
+    # Connect countdown timer signals
+    _countdown_timer.on_one_second_hit.connect(_on_one_sec_hit_timer)
+    _countdown_timer.on_countdown_timeout.connect(_on_countdown_timeout_timer)
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("quit"):
-		quit_game()
-	if event.is_action_pressed("restart"):
-		_on_user_requested_start_game()
-	if event.is_action_pressed("open_menu"):
-		pause_game()
+    if event.is_action_pressed("quit"):
+        quit_game()
+    if event.is_action_pressed("restart"):
+        _on_user_requested_start_game()
+    if event.is_action_pressed("open_menu"):
+        pause_game()
 
 
-func _process(delta: float) -> void:
-	var data = clock.get_time()
-	var msg = data.convert_date_time_to_string_msc()
-	ui.time_elapsed_update(msg)
-	# NOTE(klek): Should we draw the UI elements related to splits
-	# again each time they have been updated?
-	# If we got another time split add it to the list
-	if ui.time_splits_size() < _time_splits.size():
-		ui.time_splits_add( _time_splits[-1].convert_date_time_to_string_msc() )
-	# Here we will always redraw the entire split list, is this really
-	# necessary? Because if it is, we should always do this?
-	else :
-		ui.time_splits_reset()
-		for c in _time_splits:
-			ui.time_splits_add(c.convert_date_time_to_string_msc())
+func _process(_delta: float) -> void:
+    var data : date_time = clock.get_time()
+    var msg : String = data.convert_date_time_to_string_msc()
+    ui.time_elapsed_update(msg)
+
+    # If _time_splits.size() is different than our UI time splits
+    # size
+    # Then we redo the UI time splits
+    if ui.time_splits_size() != _time_splits.size():
+        ui.time_splits_reset()
+        for c in _time_splits:
+            ui.time_splits_add(c.convert_date_time_to_string_msc())
 
 
 func pause_game():
-	get_tree().paused = true
+    get_tree().paused = true
 
 
 func unpause_game():
-	get_tree().paused = false
+    get_tree().paused = false
 
 
 func reload_game() -> void:
-	# Load the world we are gonna use
-	# If the world has already been loaded, we change the scene
-	if !_node_name_world.is_empty():
-		# Remove the old world
-		var node_world : Node = get_node(_node_name_world)
-		remove_child(node_world)
-		node_world.queue_free()
-		# Set the string value to zero
-		_node_name_world = ""
+    # Load the world we are gonna use
+    # If the world has already been loaded, we change the scene
+    if !_node_name_world.is_empty():
+        # Remove the old world
+        var node_world : Node = get_node(_node_name_world)
+        remove_child(node_world)
+        node_world.queue_free()
+        # Set the string value to zero
+        _node_name_world = ""
 
-	if !_node_name_player.is_empty():
-		# Remove the old player
-		var node_player : Node = get_node(_node_name_player)
-		remove_child(node_player)
-		node_player.queue_free()
-		# Set the string value to zero
-		_node_name_player = ""
+    if !_node_name_player.is_empty():
+        # Remove the old player
+        var node_player : Node = get_node(_node_name_player)
+        remove_child(node_player)
+        node_player.queue_free()
+        # Set the string value to zero
+        _node_name_player = ""
 
-	var scene_world : Node2D = load_scene(world_scene)
-	if ( scene_world == null ):
-		# To be replaced with a quit function
-		quit_game()
-		return
-	# Add the world as a child
-	add_child(scene_world, true)
-	# Save the node name 
-	_node_name_world = scene_world.name
-	# Connect the time split signals
-	# NOTE(klek): Here we are assuming that the scene_world has a
-	# signal named new_time
-	scene_world.new_time.connect(_on_new_time_split)
-	
-	# Grab the starting position for the player in this world
-	var start_position : Vector2 = Vector2.ZERO
-	if ( get_node(_node_name_world).has_method("get_start_position") ):
-		start_position = get_node(_node_name_world).get_start_position()
-	
-	# Load the player
-	var scene_player : Node2D = load_scene(player_scene)
-	if ( scene_player == null ):
-		# To be replaced with a quit function
-		quit_game()
-		return
-	# Set the starting position for the player
-	scene_player.global_position = start_position
-	# Add the player as a child
-	add_child(scene_player, true)
-	# Save the node name
-	_node_name_player = scene_player.name
+    var scene_world : Node2D = load_scene(world_scene)
+    if ( scene_world == null ):
+        # To be replaced with a quit function
+        quit_game()
+        return
+    # Add the world as a child
+    add_child(scene_world, true)
+    # Save the node name
+    _node_name_world = scene_world.name
+    # Connect the time split signals
+    # NOTE(klek): Here we are assuming that the scene_world has a
+    # signal named new_time
+    scene_world.new_time.connect(_on_new_time_split)
+
+    # Grab the starting position for the player in this world
+    var start_position : Vector2 = Vector2.ZERO
+    if ( get_node(_node_name_world).has_method("get_start_position") ):
+        start_position = get_node(_node_name_world).get_start_position()
+
+    # Load the player
+    var scene_player : Node2D = load_scene(player_scene)
+    if ( scene_player == null ):
+        # To be replaced with a quit function
+        quit_game()
+        return
+    # Set the starting position for the player
+    scene_player.global_position = start_position
+    # Add the player as a child
+    add_child(scene_player, true)
+    # Save the node name
+    _node_name_player = scene_player.name
 
 
 func load_scene( scene_to_load: PackedScene ) -> Node2D:
-	# Check that ui_scene is set
-	if ( scene_to_load == null ):
-		print("Exported variable ui_scene is not set...exiting!")
-		return null
-	# Load the scene
-	var scene = scene_to_load.instantiate()
-	#add_child(scene, true)
-	print("Loaded scene: ", scene.name)
-	return scene
+    # Check that ui_scene is set
+    if ( scene_to_load == null ):
+        print("Exported variable ui_scene is not set...exiting!")
+        return null
+    # Load the scene
+    var scene = scene_to_load.instantiate()
+    #add_child(scene, true)
+    print("Loaded scene: ", scene.name)
+    return scene
 
 
 func quit_game() -> void:
-	get_tree().quit()
+    get_tree().quit()
 
 #**********************************************************************
 # Callbacks for UI elements
 func _on_user_requested_quit_game() -> void:
-	quit_game()
+    quit_game()
 
 
 func _on_user_requested_resume_game() -> void:
-	unpause_game()
+    unpause_game()
 
 
 func _on_user_requested_start_game() -> void:
-	# Pause the game world
-	pause_game()
-	# Reload the world and player
-	reload_game()
-	# Restart the clock
-	clock.restart()
-	# Increment nr of tries
-	_nr_or_retries += 1
-	ui.nr_of_tries_update(_nr_or_retries)
-	# Reset the clock ui
-	ui.time_elapsed_update(clock.get_time().convert_date_time_to_string_msc())
-	# Reset time splits
-	_time_splits.clear()
-	# Reset the time splits ui
-	ui.time_splits_reset()
-	# Start countdown timer
-	_countdown_timer.start(3)
-	# Unpause the game world
-	#unpause_game()
+    # Pause the game world
+    pause_game()
+    # Reload the world and player
+    reload_game()
+    # Restart the clock
+    clock.restart()
+    # Increment nr of tries
+    _nr_or_retries += 1
+    ui.nr_of_tries_update(_nr_or_retries)
+    # Reset the clock ui
+    ui.time_elapsed_update(clock.get_time().convert_date_time_to_string_msc())
+    # Reset time splits
+    _time_splits.clear()
+    # Reset the time splits ui
+    #ui.time_splits_reset()
+    # Start countdown timer
+    _countdown_timer.start(3)
+    # Unpause the game world
+    #unpause_game()
 
 #**********************************************************************
 # Callbacks for the countdown timer during start of level
 func _on_one_sec_hit_timer(val : int ) -> void:
-	# TODO(klek): Update GUI countdown to show new value
-	ui.countdown_show("Get ready!\n" + str(val))
+    # TODO(klek): Update GUI countdown to show new value
+    ui.countdown_show("Get ready!\n" + str(val))
 
 
 func _on_countdown_timeout_timer() -> void:
-	# TODO(klek): Update GUI countdown to be hidden
-	ui.countdown_hide()
-	# Unpause the game
-	unpause_game()
-	pass
+    # TODO(klek): Update GUI countdown to be hidden
+    ui.countdown_hide()
+    # Unpause the game
+    unpause_game()
+    #pass
 
-func _on_new_time_split(body : Node2D) -> void:
-	# When a new split time is hit, store locally and add a child 
-	# to the UI element?
-	_time_splits.append( clock.get_time() )
-	print(" New split: " + str( clock.get_time().convert_date_time_to_string_msc() ) )
+func _on_new_time_split(_body : Node2D) -> void:
+    # When a new split time is hit, store locally and add a child
+    # to the UI element?
+    _time_splits.append( clock.get_time() )
+    print(" New split: " + str( clock.get_time().convert_date_time_to_string_msc() ) )
--- scenes/stop_watch/stop_watch.gd
+++ scenes/stop_watch/stop_watch.gd
@@ -13,49 +13,48 @@ var is_stopped : bool = true
 
 
 func _process(delta: float) -> void:
-	if ( !is_stopped ):
-		total_time_elapsed_seconds += delta
-		# If the current_time is used, call update function
-		if ( current_time != null ):
-			current_time.update_time(delta)
+    if ( !is_stopped ):
+        total_time_elapsed_seconds += delta
+        # If the current_time is used, call update function
+        if ( current_time != null ):
+            current_time.update_time(delta)
 
 
 ## Stops the stopwatch
 func stop() -> void:
-	is_stopped = true
+    is_stopped = true
 
 
 ## Starts the stopwatch
 func start() -> void:
-	is_stopped = false
+    is_stopped = false
 
 
 ## Stops and resets the stopwatch
 ## NOTE(klek): Calling get_time*()-functions after this will return
 ## zero
 func reset() -> void:
-	is_stopped = true
-	total_time_elapsed_seconds = 0.0
-	if ( current_time != null ):
-		current_time.reset_time()
+    is_stopped = true
+    total_time_elapsed_seconds = 0.0
+    if ( current_time != null ):
+        current_time.reset_time()
 
 
 ## Restarts the stopwatch, by first resetting it then starting it
 func restart() -> void:
-	reset()
-	start()
+    reset()
+    start()
 
 
-## Returns the total elapsed time since last reset, as a date_time 
+## Returns the total elapsed time since last reset, as a date_time
 ## class object
 ## NOTE(klek): This will return null if the user has not assigned
 ## a date_time object to the exported variable
 func get_time() -> date_time:
-	# Should I use duplicate here? When is the duplicate freed?
-	#return current_time.duplicate()
-	return current_time
+    # Should I use duplicate here? When is the duplicate freed?
+    return current_time.duplicate()
 
 
 ## Returns the total elapsed time in seconds since last reset
 func get_time_seconds() -> float:
-	return total_time_elapsed_seconds
+    return total_time_elapsed_seconds
